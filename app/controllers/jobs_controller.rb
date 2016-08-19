f9a6963976f25ddc5a2654dd006695a8308e52bb
Frankhu <f55665566@gmail.com>
2016-08-19 15:01:49 +0800

    categories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,13 @@ class JobsController < ApplicationController
 
 
   def index
-    @jobs = Job.all.order("created_at DESC")
+    if params[:category].blank?
+      @jobs = Job.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
+
   end
 
   def show
@@ -47,7 +53,7 @@ class JobsController < ApplicationController
   end
 
   def jobs_params
-    params.require(:job).permit(:title, :description, :company, :url)
+    params.require(:job).permit(:title, :description, :company, :url, :category_id)
   end
 
 
